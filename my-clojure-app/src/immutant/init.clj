9c7038fc0ded071e266ca84acea7a25df9b436ec
Jim Crossley <jim@crossleys.org>
2013-04-01 17:41:47 -0400

    Redirect needs to be context-aware

--- my-clojure-app/src/immutant/init.clj
+++ my-clojure-app/src/immutant/init.clj
@@ -1,9 +1,8 @@
 (ns immutant.init
-  (:require immutant.web)
-  (:use [ring.middleware.resource :only [wrap-resource]]
-        [ring.util.response :only [redirect]]))
+  (:require [immutant.web :as web])
+  (:use [ring.util.response :only [redirect]]))
 
 (defn handler [request]
-  (redirect "/index.html"))
+  (redirect (str (:context request) "/index.html")))
 
-(immutant.web/start (wrap-resource handler "public"))
+(web/start (web/wrap-resource handler "public"))
